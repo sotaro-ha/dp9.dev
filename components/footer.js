91d942485e4fd45a43b1b1e0d39b62ae357062e7
横井総太朗 <alphacomplex.104729@gmail.com>
2023-03-31 23:10:41 +0900

    resolve lower case

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-function Footer() {
+export default function Footer() {
     return (
         <footer className="bg-white rounded-lg dark:bg-gray-900 m-4">
             <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
@@ -31,4 +31,3 @@ function Footer() {
     );
 }
 
-export default Footer;
