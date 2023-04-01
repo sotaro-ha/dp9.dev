b91018e2533789776401ee1612db24c8eafe7521
yokoi <yokoi@au-sphere.com>
2023-04-02 04:55:07 +0900

    add gtag

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,23 @@ import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
 import Head from "next/head";
 import usePagePoints from "../hooks/usePagePoints";
+import Script from "next/script";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import * as gtag from "src/lib/gtag";
 
 function MyApp({ Component, pageProps }) {
   const points = usePagePoints();
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouterChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouterChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouterChange);
+    };
+  }, [router.events]);
   return (
     <>
       <Head>
@@ -14,6 +28,23 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+           window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+
+           gtag('config', '${gtag.GA_MEASUREMENT_ID}');
+           `,
+        }}
+      />
       <Component {...pageProps} points={points} />
     </>
   );
