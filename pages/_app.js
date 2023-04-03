1c942dc2fb77c4d9e94374536e1a37fe4b621da0
yokoi <yokoi@au-sphere.com>
2023-04-03 23:52:44 +0900

    add meta tag

--- pages/_app.js
+++ pages/_app.js
@@ -22,11 +22,60 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>dp9.dev | Web tutorial by designing plus nine</title>
+        <title>
+          dp9.dev | Design by Development | dp9によるWeb制作チュートリアルです
+        </title>
         <meta name="description" content="Web制作を学ぶためのチュートリアル" />
-        <meta charSet="utf-8" />
+        <meta charSet="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.dp9.dev/" />
+        <meta
+          property="og:title"
+          content="dp9.dev | Web tutorial by designing plus nine"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff"></meta>
+        <link rel="manifest" href="https://www.dp9.dev/manifest.json" />
+        <meta
+          property="og:description"
+          content="designing plus nineが提供する、Web初心者のためのWebチュートリアル"
+        />
+        <meta property="og:image" content="https://www.dp9.dev/ogp.png" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://www.dp9.dev/" />
+        <meta
+          property="twitter:title"
+          content="dp9.dev | Web tutorial by designing plus nine"
+        />
+        <meta
+          property="twitter:description"
+          content="designing plus nineが提供する、Web初心者のためのWebチュートリアル"
+        />
+        <meta property="twitter:image" content="https://www.dp9.dev/ogp.png" />
       </Head>
       <Script
         strategy="afterInteractive"
